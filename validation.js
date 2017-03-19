21ea3ff6c03fe61db7a48893b867c5c03cf3e5a5
neha8381 <nehasharma8381@gmail.com>
2017-03-19 18:07:15 -0400

    added validations to Firstname,lastname,username and email address

--- validation.js
+++ validation.js
@@ -1,19 +1,79 @@
-function check(FirstName)
+
+function registrationValidation()
 {
-  var a=/^[A-Z0-9]+$/;
+  var fname=document.signin.FirstName;
+  var lname=document.signin.LastName;
+  var uname=document.signin.username;
+  var email=document.signin.email;
+if(username_validation(uname))
+{
+if(email_validation(email))
+{
+if(fname_validation(fname))
+{
+if(lname_validation(lname))
+{
 
-
-  if(FirstName.value.match(a))
+}
+}
+}
+}
+return false;
+}
+function fname_validation(fname)
+{
+  var letters=/^[A-Za-z0-9]+$/;
+  if(fname.value.match(letters) )
   {
-    alert("success");
-
-  }
-
-  if (LastName.value.match(a))
-  {
-      alert("success");
+    return true;
   }
   else {
-    alert("Invalid LastName");
+    {
+      alert("First name can only A-Z 0-9");
+      fname.focus();
+    }
   }
+}
+function lname_validation(lname)
+{
+  var letters=/^[A-Za-z0-9]+$/;
+  if(lname.value.match(letters))
+  {
+    return true;
+  }
+  else {
+    {
+      alert("First name can only A-Z 0-9");
+      lname.focus();
+    }
+  }
+}
+function username_validation(uname)
+{
+var letters = new RegExp("[a-zA-Z0-9_-]");
+var username_length=uname.value.length;
+if(uname.value.match(letters) && username_length>=5)
+{
+return true;
+}
+else
+{
+alert("Username must have alphabet characters only");
+uname.focus();
+return false;
+}
+}
+function email_validation(email)
+{
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(email.value.match(mailformat))
+{
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+email.focus();
+return false;
+}
 }
